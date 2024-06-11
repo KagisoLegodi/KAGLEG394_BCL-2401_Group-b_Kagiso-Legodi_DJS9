7edd0e731163017efc31a9178851dfaa6d373320
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-11 16:13:48 +0200

    installed npx webpack init

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+const returningUserDisplay = document.querySelector("#returning-user");
+const userNameDisplay = document.querySelector("#user");
 const reviewTotalDisplay = document.querySelector("#reviews");
 
 const reviews = [
@@ -21,7 +23,6 @@ const reviews = [
   },
 ];
 
-// Solution
 function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
   const iconDisplay = isLoyalty ? "⭐" : "";
   reviewTotalDisplay.innerHTML =
@@ -34,3 +35,17 @@ function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+function populateUser(isReturning: boolean, userName: string) {
+  if (isReturning) {
+    returningUserDisplay.innerHTML = "back";
+  }
+  userNameDisplay.innerHTML = userName;
+}
+
+populateUser(you.isReturning, you.userName);
